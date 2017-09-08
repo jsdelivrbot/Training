591d3a3d9bc49c074ca45393f0258bb87669ecc1
Seth Borne <sethborne@gmail.com>
2017-09-08 16:11:01 -0700

    Training - Udemy - Angular 4 - Section 02 - 28 - First Commit

---
+++ Udemy/Angular 4 - The Complete Guide/Section 02 - The Basics/28 - Passing and Using Data with Event Binding/ProjectLecture28/src/app/servers/servers.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+
+  serverCreationStatus: string = "No Server Was Created!"
+
+  serverName: string = "";
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server Was Created'
+  }
+
+  onUpdateServerName(event: Event){
+    // console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
