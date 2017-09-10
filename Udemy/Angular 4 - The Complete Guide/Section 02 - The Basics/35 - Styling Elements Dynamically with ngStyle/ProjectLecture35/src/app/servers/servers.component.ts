e22ff6d6750bba80233370bb059bb9dcd5bda8a2
Seth Borne <sethborne@gmail.com>
2017-09-10 11:58:31 -0700

    Training - Udemy - Angular 4 - Section 02 - 35 - First Commit

---
+++ Udemy/Angular 4 - The Complete Guide/Section 02 - The Basics/35 - Styling Elements Dynamically with ngStyle/ProjectLecture35/src/app/servers/servers.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+  serverCreationStatus: string = "No Server Was Created!"
+  serverName: string = "";
+  serverCreated: boolean = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server Was Created.  Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event){
+    // console.log(event);
+
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
