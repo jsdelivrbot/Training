0e5de1ce04efae2d794ef15ce9b0914f80a427f4
Seth Borne <sethborne@gmail.com>
2017-09-14 15:18:39 -0700

    Training - Udemy - JavaScript - Understanding the Weird Parts - Lecture 42

---
+++ Udemy/Javascript_Understanding the Weird Parts/Lecture 42/app.js
@@ -0,0 +1,13 @@
+function getPerson(){
+    // doesn't work
+    // return 
+    // {
+    //     firstname: "Tony"
+    // }
+    
+    return {
+        firstname: "Tony"
+    }
+}
+
+console.log(getPerson());
