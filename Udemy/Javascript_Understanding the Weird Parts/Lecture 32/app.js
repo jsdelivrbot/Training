2c6cb0d4e141187f7e255ae7e07445639a493ba4
Seth Borne <sethborne@gmail.com>
2017-09-14 15:15:33 -0700

    Training - Udemy - JavaScript - Understanding the Weird Parts - Lecture 32

---
+++ Udemy/Javascript_Understanding the Weird Parts/Lecture 32/app.js
@@ -0,0 +1,21 @@
+// namespace
+// container for variables and functions
+// to keep var
+
+var greet = "Hello";
+var greet = "Hola";
+
+console.log(greet);
+
+var english = {
+    greetings: {
+        greet: ""
+    }
+};
+var spanish = {};
+
+english.greetings.greet = "Hello!";
+spanish.greet = "Hola!"
+
+console.log(english.greetings.greet);
+console.log(spanish.greet);
