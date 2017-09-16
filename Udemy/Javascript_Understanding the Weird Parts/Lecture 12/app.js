5d2215afe4649dafb46a86c83a089d2965a32bf8
Seth Borne <sethborne@gmail.com>
2017-09-15 20:34:35 -0700

    Training - Udemy - JavaScript - Understanding the Weird Parts - Lecture 12

---
+++ Udemy/Javascript_Understanding the Weird Parts/Lecture 12/app.js
@@ -0,0 +1,31 @@
+// var a = 'Hello World';
+
+// function b(){
+    // console.log('Called B');
+// };
+
+// Step 1
+// expected result
+
+// b();
+// console.log(a);
+
+// expect b call then 
+// log a
+
+function b(){
+    console.log('Called B');
+};
+
+b();
+
+
+console.log(a);
+
+var a = 'Hello World';
+
+console.log(a);
+
+//called b
+// undefined
+// hello world
