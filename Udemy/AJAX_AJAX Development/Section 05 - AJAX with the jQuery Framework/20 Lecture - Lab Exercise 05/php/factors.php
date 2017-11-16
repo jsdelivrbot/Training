36cebe4fbbbc4bbed7a6797e179c2badf2a66df7
Seth Borne <sethborne@gmail.com>
2017-11-15 21:50:19 -0800

    Training - Udemy - AJAX - AJAX Development - Section 05 - First Commit

---
+++ Udemy/AJAX_AJAX Development/Section 05 - AJAX with the jQuery Framework/20 Lecture - Lab Exercise 05/php/factors.php
@@ -0,0 +1,16 @@
+<?php
+    $num1 = $_REQUEST['number'];
+    print("Number is: " . $num1);
+    // $num1 = $_GET['number'];
+    
+    // $document = "";
+    
+    // for($x=0; $x < ($num1+1); $x++)
+    // {
+    //     if($num1 % $x ==0)
+    //     {
+    //         print($x);
+    //         print("<br/>");
+    //     }
+    // }
+?>
