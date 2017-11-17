2dc149d9c3872a03070b747865d7927fd60c4064
Seth Borne <sethborne@gmail.com>
2017-11-17 11:44:42 -0800

    Training - Udemy - JavaScript - Algorithms and Data Structures - Section 04 - Palindrome Solutions - First Commit

---
+++ Udemy/Javascript_CIB Algorithms and Data Structures/04 Section - Palindromes/13 Lecture - Palindromes/index.js
@@ -0,0 +1,19 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+function palindrome(inputString) {
+    let isPalindrome = false;
+    let reversedString = inputString.split("").reverse().join("");
+    if(inputString === reversedString){
+        isPalindrome = true;
+    }
+    return isPalindrome;
+}
+
+module.exports = palindrome;
