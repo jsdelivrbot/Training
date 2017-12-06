8da91a02fb4f578ea259fba1dbbd06fdddff54db
Seth Borne <sethborne@gmail.com>
2017-12-06 09:35:27 -0800

    Training - Udemy - JavaScript - Understanding the Weird Parts - Lecture 74 - First Commit

---
+++ Udemy/Javascript_Understanding the Weird Parts/Lecture 74/Greetr.js
@@ -0,0 +1,7 @@
+// create new execution context for framework/library - so all variables are safe and only exposing to global what we want
+// start - wrap some code to make safe, and pass what we need access too, global and $
+
+( function( global, $ ){
+    // 
+    }
+(window, jQuery));
