56af4ed77f7d64be14576707513e976489ff729e
Seth Borne <sethborne@gmail.com>
2017-12-05 20:49:15 -0800

    Training - Udemy - JavaScript - Understanding the Weird Parts - Lecture 60 - First Commit

---
+++ Udemy/Javascript_Understanding the Weird Parts/Lecture 60/app.js
@@ -0,0 +1,21 @@
+// var a = new Number(3);
+// console.log(a);
+
+// var s = new String("John");
+// console.log(s);
+
+// console.log(s.indexOf("o"));
+
+String.prototype.isLengthGreaterThan = function(limit){
+    return this.length > limit;
+}
+
+console.log("John".isLengthGreaterThan(3));
+
+Number.prototype.isPositive = function(){
+    return this > 0;
+}
+
+// console.log(3.isPositive());
+var a = new Number(3);
+a.isPositive();
