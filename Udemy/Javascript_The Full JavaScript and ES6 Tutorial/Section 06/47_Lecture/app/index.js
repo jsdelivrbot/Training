15bf69c8fa3f83cacfd4eb1b2d8be4dca08c30c9
Seth Borne <sethborne@gmail.com>
2017-12-12 10:17:39 -0800

    Training - Udemy - JavaScript - The Full JavaScript and ES6 - Lecture 47 - First Commit

---
+++ Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 06/47_Lecture/app/index.js
@@ -0,0 +1,23 @@
+// function cheer(){
+//     console.log("Yeaaaaaaaaa!");
+// }
+
+// var cheer = function(){
+//     console.log("Yeaaaaaaaaa!");
+// }
+// anonymous function expression
+
+// setTimeout(function(){
+//     console.log("After 3 seconds.");
+// }, 3000);
+
+// setTimeout(() => {
+//     console.log("After 3 seconds.");
+// }, 3000);
+
+let cheer = () => {
+    console.log("Anon Cheer");
+}
+// anonymous arrow function expression
+
+cheer();
