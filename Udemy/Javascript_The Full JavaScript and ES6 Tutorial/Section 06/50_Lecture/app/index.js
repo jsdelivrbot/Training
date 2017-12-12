d7db316e694f266f257e506b92aeb17603815767
Seth Borne <sethborne@gmail.com>
2017-12-12 10:19:09 -0800

    Training - Udemy - JavaScript - The Full JavaScript and ES6 - Lecture 50 - First Commit

---
+++ Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 06/50_Lecture/app/index.js
@@ -0,0 +1,15 @@
+// let b = "wooh" + "oo".repeat(50);
+// let b = "wooh" + " ".repeat(50) + "oo";
+// let b = `woo${"oo".repeat(50)}`;
+// console.log(b);
+
+
+console.log("butterfly".startsWith("butter"));
+console.log("butterfly".startsWith("fly"));
+console.log("butterfly".endsWith("fly"));
+
+console.log("");
+
+console.log("butterfly".includes("butter"));
+console.log("butterfly".includes("fly"));
+console.log("butterfly".includes("not"));
