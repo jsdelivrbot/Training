f031b5c92070a05b8c00e56f9a87e16e31db9f51
Seth Borne <sethborne@gmail.com>
2017-12-12 10:19:37 -0800

    Training - Udemy - JavaScript - The Full JavaScript and ES6 - Lecture 52 - First Commit

---
+++ Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 06/52_Lecture/app/index.js
@@ -0,0 +1,9 @@
+import { fellowship, total } from './fellowship';
+// import { add, multiply } from "./math";
+import multiply from "./math";
+
+console.log(add(5, 10));
+console.log(multiply(5, 10));
+
+console.log(fellowship);
+console.log(total);
