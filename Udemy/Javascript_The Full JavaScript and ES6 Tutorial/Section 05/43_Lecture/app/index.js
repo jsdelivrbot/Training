7c1b6b6961ad1b35e12201902ec95840221aa6ba
Seth Borne <sethborne@gmail.com>
2017-12-11 18:01:10 -0800

    Training - Udemy - JavaScript - The Full JavaScript and ES6 - Lecture 43 - First Commit

---
+++ Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 05/43_Lecture/app/index.js
@@ -0,0 +1,15 @@
+// let a = [ 20, 30, 40 ];
+// let b = [10, ...a, 50];
+// console.log(b);
+
+// let a = [ "Dana", "Erik", "Frank"];
+// let b = [ "Alice", "Bob", "Carl", ...a ];
+
+// console.log(b);
+
+function collect(...a){
+    console.log(a);
+}
+
+// collect(1, 2, 3, 4);
+collect(3, 6, 9, 12, 15);
