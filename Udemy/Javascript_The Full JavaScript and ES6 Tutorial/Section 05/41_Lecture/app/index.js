5e6cb05bedd2b07a00642af2e6409138d3f0beea
Seth Borne <sethborne@gmail.com>
2017-12-11 18:00:58 -0800

    Training - Udemy - JavaScript - The Full JavaScript and ES6 - Lecture 41 - First Commit

---
+++ Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 05/41_Lecture/app/index.js
@@ -0,0 +1,9 @@
+// let a = `good`;
+// let greeting = `good morning`
+// console.log(a);
+// console.log(greeting);
+// console.log(`${a} ${greeting}`);
+
+let b = "Birthday";
+let c = `Happy ${b}`
+console.log(c);
