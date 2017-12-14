b90c30420f4dfd276ba282363a5bb15d0e9c3f94
Seth Borne <sethborne@gmail.com>
2017-12-13 20:07:10 -0800

    Training - Udemy - JavaScript - The Full JavaScript and ES6 - Lecture 57 - First Commit

--- Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 07/57_Lecture/app/index.js
+++ Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 07/57_Lecture/app/index.js
@@ -1,31 +1,16 @@
-// class Animal {
-//     constructor(name, height){
-//         this.name = name;
-//         this.height = height;
-//     }
-    
-//     hello(){ console.log(`I'm ${this.name} from the land!`)};
-// }
-import { Animal } from './Animal';
-import { Lion } from './Lion'
+class Calculator{
+    static multiply(a, b){
+        return a * b;
+    }
+    static add(a, b){
+        return a + b;
+    }
+}
 
+let a = Calculator.multiply(5, 7);
 
-let generic = new Animal("Tim", 180);
-console.log(generic);
-generic.hello();
+console.log(a);
 
-// class Lion extends Animal {
-//     constructor(name, height, color){
-//         // get access to parent
-//         super(name, height);
-//         // extended class attribute
-//         this.color = color;
-//     }
-//     hello(){ console.log(`I'm ${this.name} from the Castle Rock!`)};
-// }
+let b = Calculator.add(5, 7);
 
-let newLion = new Lion("Lion", 250, "Golden")
-console.log(newLion);
-newLion.hello();
-
-// classes are usually in their own modules
+console.log(b);
