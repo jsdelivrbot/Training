2844f6202aaf5cfe0f585f3b97c8be2393e9e286
Seth Borne <sethborne@gmail.com>
2017-12-13 21:34:47 -0800

    Training - Udemy - JavaScript - The Full JavaScript and ES6 - Lecture 79 - First Commit

---
+++ Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 10/79_Lecture/app/index.js
@@ -0,0 +1,17 @@
+function* evens(){
+    let count = 0;
+    while(true){
+        count += 2;
+        let reset = yield count;
+        if(reset){
+            count = 0;
+        }
+    }
+}
+
+let sequence = evens();
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next(true).value);
+console.log(sequence.next().value);
