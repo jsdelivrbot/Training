dedf82f1ba6936e5489829edc9d5a61bd182d03b
Seth Borne <sethborne@gmail.com>
2017-12-14 10:53:15 -0800

    Training - Udemy - JavaScript - The Full JavaScript and ES6 - Lecture 88 - First Commit

---
+++ Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 12/88_Lecture/app/index.js
@@ -0,0 +1,10 @@
+// let a = Math.pow(2, 5);
+// let a = 2**5;
+// console.log(a);
+
+// let b = 'wonderful'.includes('wonder');
+// let b = 'wonderful'.includes('butter');
+// console.log(b);
+
+let c = [2, 3, 4, 5, 6].includes(3);
+console.log(c);
