fa2224b4a69af3d99f0f23dc856c3929da60adfc
Seth Borne <sethborne@gmail.com>
2017-12-15 13:25:50 -0800

    Training - Udemy - JavaScript - The Full JavaScript and ES6 - Lecture 94 - First Commit

---
+++ Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 13/94_Lecture/app/components/Global.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap'
+
+class Global extends Component {
+    render(){
+        // people forget the return
+        return (
+            <div>
+                The Global App Component
+            </div>
+        )
+    }
+}
+
+// export
+export default Global;
