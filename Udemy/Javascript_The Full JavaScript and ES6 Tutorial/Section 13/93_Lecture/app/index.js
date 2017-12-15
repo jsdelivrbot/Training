934f5fe85dda0123205810148607e32a14ddbb5d
Seth Borne <sethborne@gmail.com>
2017-12-15 13:24:59 -0800

    Training - Udemy - JavaScript - The Full JavaScript and ES6 - Lecture 93 - Second Commit

--- Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 13/93_Lecture/app/index.js
+++ Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 13/93_Lecture/app/index.js
@@ -1,31 +1,8 @@
-async function asyncOne(){
-    return 'one';
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Global from './components/Global'
 
-// asyncOne().then(response => console.log(response))
-
-async function asyncTwo(){
-    // throw new Error('Issue with Async!');
-    return 'two';
-}
-
-asyncTwo().catch(error => console.log(error));
-
-async function asyncThree(){
-    // const one = await setTimeout(asyncOne(), 3000);
-    const one = await asyncOne();
-    console.log(one);
-    const two = await asyncTwo();
-    console.log(two);
-}
-
-asyncThree();
-
-async function asyncFour(){
-    const [resOne, resTwo] = await Promise.all(
-        [asyncOne(), asyncTwo()]
-    )
-    console.log(resOne, resTwo);
-}
-
-asyncFour();
+ReactDOM.render (
+    // this is JSX
+    <div>React Application!</div>, document.getElementById('root')
+)
