9ab2b72b18702935ceb97763761c30cf117d94cf
Seth Borne <sethborne@gmail.com>
2017-12-15 13:27:09 -0800

    Training - Udemy - JavaScript - The Full JavaScript and ES6 - Lecture 97 - First Commit

---
+++ Udemy/Javascript_The Full JavaScript and ES6 Tutorial/Section 13/97_Lecture/app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Global from './components/Global'
+
+ReactDOM.render (
+    // this is JSX
+    <Global />, document.getElementById('root')
+)
