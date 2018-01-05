c6b399af4624b8260d15c87530956ebfbd287b2c
Seth Borne <sethborne@gmail.com>
2018-01-04 17:17:24 -0800

    Training - Lynda - JavaScript - Learning Functional Programming - Section 05 - Lecture 03 - First Commit

---
+++ Lynda/JavaScript_Learn Functional Programming with JavaScript/05_Section/03_Lecture/01_loop.js
@@ -0,0 +1,13 @@
+// Learning Functional Programming with Javascript
+// Chapter 05, Video 03, Exercise 01
+
+function loop(i) {
+    console.log("i is " + i);
+    if (i < 10){
+        // loop(i - 1);
+        loop(i + 1);
+    }
+}
+
+// loop(10);
+loop(0);
