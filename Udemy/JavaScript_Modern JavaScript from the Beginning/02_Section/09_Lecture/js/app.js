fe43932f7d18773bb74245bec9d64f6031bc69c9
Seth Borne <sethborne@gmail.com>
2018-01-10 08:34:36 -0800

    Training - Udemy - JavaScript - Modern JavaScript from the Beginning - Section 02 - Lecture 09 - First Commit

---
+++ Udemy/JavaScript_Modern JavaScript from the Beginning/02_Section/09_Lecture/js/app.js
@@ -0,0 +1,34 @@
+const num1 = 100;
+const num2 = 50;
+
+let val;
+
+// simple math with numbers
+
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.8);
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(4);
+val = Math.pow(2, 2);
+val = Math.abs(-3);
+
+val = Math.min(2, 3, 5, 6, 7)
+val = Math.max(2, 3, 4, 5, 6);
+
+val = Math.random()
+
+// 0 to 10
+val = Math.ceil(Math.random() * 10)
+
+console.log(val);
