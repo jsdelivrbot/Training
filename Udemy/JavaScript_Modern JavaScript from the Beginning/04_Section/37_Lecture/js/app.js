556c3af68d0e06ec5bbc74a4b6e433cf93111bb7
Seth Borne <sethborne@gmail.com>
2018-01-18 14:37:36 -0800

    Training - Udemy - JavaScript - Modern JavaScript from the Beginning - Section 04 - Lecture 37 - First Commit

---
+++ Udemy/JavaScript_Modern JavaScript from the Beginning/04_Section/37_Lecture/js/app.js
@@ -0,0 +1,95 @@
+// Event Listener
+
+// capture the form
+const form = document.getElementById('loan-form');
+// invoke listeners
+allEventListeners()
+
+function allEventListeners(){
+    form.addEventListener('submit', calculateResults)
+}
+
+function calculateResults(event){
+    // becasue its a form prevent default
+    console.log("Calculating...");
+    // UI Variables
+    const amountElement = document.getElementById('amount');
+    // console.log(amountElement);
+    const interestElement = document.getElementById('interest');
+    // console.log(interestElement);
+    const yearsElement = document.getElementById('years');
+    // console.log(yearsElement);
+    //app.use(bp.urlencoded({extended:true}));
+    // UI Variables Display
+    const monthlyPaymentElement = document.getElementById('monthlyPayment');
+    // console.log(monthlyPaymentElement);
+    const totalPaymentElement = document.getElementById('totalPayment');
+    // console.log(totalPaymentElement);
+    const totalInterestElement = document.getElementById('totalInterest');
+    // console.log(totalInterestElement);
+    
+    // Calculations
+    const principal = parseFloat(amountElement.value);
+    // console.log(principal);
+    const calculatedInterest = parseFloat(interestElement.value) / 100 / 12;
+    
+    const calculatedPayments = parseFloat(yearsElement.value) * 12;
+    
+    // compute Monthly Payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthlyPay = (principal*x*calculatedInterest)/(x-1);
+    
+    if(isFinite(monthlyPay)){
+        monthlyPaymentElement.value = monthlyPay.toFixed(2);
+        totalPaymentElement.value = (monthlyPay * calculatedPayments).toFixed(2);
+        totalInterestElement.value = ((monthlyPay * calculatedPayments)-principal).toFixed(2);
+    }
+    else {
+        // console.log("Please Check Your numbers, cause it didn't work");
+        showError('Please check your Numbers');
+    }
+    event.preventDefault();
+}
+
+function showError(errorStr){
+    // Create a div
+    const errorDiv = document.createElement('div');
+    // Get Elements
+    // html collection - getElementsByClassName
+    const cardElement = document.querySelector('.card');
+    // returns the Element - querySelector
+    // console.log(cardElement);
+    const headingElement = document.querySelector('.heading')
+    
+    // Add class
+    errorDiv.className = 'alert alert-danger';
+    
+        // add a close button "x" to this alert
+        const buttonElement = document.createElement('button');
+        buttonElement.className = 'close'
+        buttonElement.setAttribute('type', 'button');
+        buttonElement.setAttribute('data-dismiss', 'alert');
+        buttonElement.setAttribute('aria-label', 'close');
+            // this is a span Element inside the button element per bootsrap
+            const spanElement = document.createElement('span');
+            spanElement.setAttribute('aria-hidden', 'true');
+            spanElement.innerHTML = '&times;'
+            
+            buttonElement.appendChild(spanElement);
+    
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(errorStr));
+    errorDiv.appendChild(buttonElement)
+    
+    cardElement.insertBefore(errorDiv, headingElement);
+    // in window, do a set timeout
+    // Clear Error after 3 seconds
+    setTimeout(clearError, 30000);
+}
+
+// Clear Error
+
+function clearError(){
+    const alertDiv = document.querySelector('.alert');
+    alertDiv.remove();
+}
