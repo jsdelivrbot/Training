a460989588c987b849192dc3daabd503f8a4a07a
Seth Borne <sethborne@gmail.com>
2018-03-14 18:48:01 -0700

    Training - Udemy - JavaScript - Modern JavaScript from the Beginning - Section 11 - Lecture 95 - First Commit

---
+++ Udemy/JavaScript_Modern JavaScript from the Beginning/11_Section/95_Lecture/js/app.js
@@ -0,0 +1,53 @@
+function MemberFactory(){
+    this.createMemeber = function(name, type){
+        let member;
+        
+        if(type === 'simple'){
+            member = new SimpleMembership(name);
+        }
+        else if(type === 'standard'){
+            member = new StandardMembership(name)
+        }
+        else if(type === 'super'){
+            member = new SuperMembership(name)
+        }
+        
+        member.type = type;
+        
+        member.define = function(){
+            console.log(`${this.name} (${this.type}): ${this.cost} `);
+        }
+        
+        return member;
+    }
+}
+
+const SimpleMembership = function(name){
+    this.name = name;
+    this.cost = '$5';
+}
+
+const StandardMembership = function(name){
+    this.name = name;
+    this.cost = '$15';
+}
+
+const SuperMembership = function(name){
+    this.name = name;
+    this.cost = '$25';
+}
+
+const members = [];
+
+const factory = new MemberFactory();
+
+members.push(factory.createMemeber('Seth', 'simple'));
+members.push(factory.createMemeber('Steve Jackson', 'super'));
+members.push(factory.createMemeber('Bob Builder', 'standard'));
+members.push(factory.createMemeber('Thomas Japerson', 'simple'));
+
+console.log(members);
+
+members.forEach(function(member){
+    member.define()
+})
