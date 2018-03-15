fc9a425549f47be92cea44043b4be9bfb8412464
Seth Borne <sethborne@gmail.com>
2018-03-14 18:48:23 -0700

    Training - Udemy - JavaScript - Modern JavaScript from the Beginning - Section 11 - Lecture 96 - First Commit

---
+++ Udemy/JavaScript_Modern JavaScript from the Beginning/11_Section/96_Lecture/js/appES6.js
@@ -0,0 +1,70 @@
+class EventObserver {
+    constructor(){
+        this.observers = [];
+    }
+    subscribe(fn){
+        this.observers.push(fn);
+        console.log(`You are now subscribed to ${fn.name}`);
+    }
+    unsubscribe(fn){
+        // filter out from the list whatever matches the callback function.  if there is no match, the callback gets to stay on the list.  the filter returns a new list and reassigns teh list of observers
+        this.observers = this.observers.filter(function(item){
+            // if item not equal to function 
+            if(item !== fn){
+                return item
+            }
+        });
+        
+        console.log(`You are not unsubscribed from ${fn.name}`);
+    }
+    fire(){
+        this.observers.forEach(function(item){
+            item.call();
+        })
+    }
+}
+// EventObserver.prototype = {
+//     subscribe: function(fn){
+        
+//     },
+//     unsubscribe: function(fn){
+        
+//     },
+//     fire: function(){
+        
+//     }
+// }
+
+const click = new EventObserver();
+
+// event Listeners
+
+document.querySelector('.sub-ms').addEventListener('click', function(){
+    click.subscribe(getCurMilliseconds);
+});
+
+document.querySelector('.unsub-ms').addEventListener('click', function(){
+    click.unsubscribe(getCurMilliseconds);
+});
+
+document.querySelector('.sub-s').addEventListener('click', function(){
+    click.subscribe(getCurSeconds);
+});
+
+document.querySelector('.unsub-s').addEventListener('click', function(){
+    click.unsubscribe(getCurSeconds);
+});
+
+document.querySelector('.fire').addEventListener('click', function(){
+    click.fire();
+});
+
+const getCurMilliseconds = function(){
+    console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+}
+
+// click Handler
+
+const getCurSeconds = function(){
+    console.log(`Current Seconds: ${new Date().getSeconds()}`);
+}
