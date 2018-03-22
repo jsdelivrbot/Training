dcb23c3f8cd64f41f0c14da54f1d310a236c1e9a
Seth Borne <sethborne@gmail.com>
2018-03-22 16:03:12 -0700

    Training - Udemy - JavaScript - Modern JavaScript from the Beginning - Section 13 - Lecture 112 - First Commit

---
+++ Udemy/JavaScript_Modern JavaScript from the Beginning/13_Section/112_Lecture/src/app.js
@@ -0,0 +1,26 @@
+// const greeting = 'Hello World';
+// console.log(greeting);
+
+// common JS module syntax
+// const person = require('./myModule1')
+// console.log(person.name);
+// console.log(person.email);
+
+// ES2015 module
+
+// import {
+//     person, sayHello 
+// } from './myModule2015'
+
+// console.log(person.name);
+// console.log(sayHello());
+
+
+// import * as mod from './myModule2015'
+
+// console.log(mod.person.name);
+// console.log(mod.sayHello());
+
+import greeting from './myModule2015';
+
+console.log(greeting);
