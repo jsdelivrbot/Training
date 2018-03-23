0093d9c1fc6047a7742f12dd8e59b5b4444a2fd1
Seth Borne <sethborne@gmail.com>
2018-03-23 08:52:46 -0700

    Training - Udemy - JavaScript - Complete Nodejs Dev Course - Section 03 - Lecture 11 - First Commit

---
+++ Udemy/JavaScript_The Complete Nodejs Developer Course_2nd Edition/03_Section/11_Lecture/notes.js
@@ -0,0 +1,13 @@
+console.log("Starting Notes .js ...");
+
+// console.log(module);
+
+// module.exports.age = 40;
+module.exports.addNote = () => {
+    console.log("addNote");
+    return 'New Note'
+}
+
+module.exports.add = (num1, num2) => {
+    return num1 + num2;
+}
