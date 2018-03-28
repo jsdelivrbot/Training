6305164ca007a279edc5a4b632b8fb60aa1fefe5
Seth Borne <sethborne@gmail.com>
2018-03-27 20:54:15 -0700

    Training - Udemy - JavaScript - Complete Nodejs Dev Course - Section 03 - Lecture 27 - First Commit

---
+++ Udemy/JavaScript_The Complete Nodejs Developer Course_2nd Edition/04_Section/27_Lecture/sandbox/callStackEventQueue.js
@@ -0,0 +1,14 @@
+// var x = 1;
+
+// var y = x + 9;
+
+// console.log(`y is ${y}`);
+
+var add = (a, b) => {
+    var total = a + b;
+    return total;
+}
+
+var res = add(3, 8);
+
+console.log(res);
