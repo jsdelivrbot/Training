13653918ebd7a6f385d7747719ccc01e9535be4b
Seth Borne <sethborne@gmail.com>
2018-03-28 21:25:56 -0700

    Training - Udemy - JavaScript - Complete Nodejs Dev Course - Section 04 - Lecture 32 - First Commit

---
+++ Udemy/JavaScript_The Complete Nodejs Developer Course_2nd Edition/04_Section/32_Lecture/app.js
@@ -0,0 +1,45 @@
+const request = require('request');
+const api_key = require('./api_key/api_key.js')
+
+const yargs = require('yargs');
+
+// node app.js --address=''
+const argv = yargs.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Please enter the address you want to Fetch Weather for',
+        string: true,
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+console.log(argv);
+const encodeAddress = encodeURIComponent(argv.address);
+
+// console.log(api_key);
+// console.log(`http://maps.googleapis.com/maps/api/geocode/json?address=2500%20N%2050th%20Street%20Seattle&${api_key.module.api}`);
+request({
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}&${api_key.api}`,
+    json: true
+    }, (error, response, body) => {
+    // error, response, body
+    // console.log(body);
+    // console.log(JSON.stringify(response.body, undefined, 2));
+    if(error){
+        console.log('Unable to Connect to Google Server.');
+    }
+    else if(body.status === 'ZERO_RESULTS'){
+        console.log('No results for your Query, Please try again');
+    }
+    else if(body.status === 'OK'){
+        console.log(`${response.body.results[0].formatted_address}`);
+        console.log('Lat: ', `${response.body.results[0].geometry.location.lat}`);
+        console.log('Long: ',  `${response.body.results[0].geometry.location.lng}`);
+    }
+    // else {
+    // }
+    
+})
