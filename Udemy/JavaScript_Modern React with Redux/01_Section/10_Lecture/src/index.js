cf4538e32da5403f659ffa8aa5caff06ed887bb5
Seth Borne <sethborne@gmail.com>
2018-04-22 16:35:31 -0700

    Training - Udemy - JavaScript - Modern React with Redux - Section 01 - Lecture 10 - First Commit

---
+++ Udemy/JavaScript_Modern React with Redux/01_Section/10_Lecture/src/index.js
@@ -0,0 +1,14 @@
+// get React as a JS module
+import React from 'react';
+// reactDOm
+import ReactDOM from 'react-dom'
+
+// create a new component.  this component should produce some HTML
+
+const App = function(){
+    return <div>Hi!</div>
+}
+
+// take this components generated HTML and put it in the DOM
+// React render App
+ReactDOM.render(<App />);
