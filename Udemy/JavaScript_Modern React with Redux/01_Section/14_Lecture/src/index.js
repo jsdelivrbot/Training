43e9f96eea88a50127f755fdaa65c288603ef4cb
Seth Borne <sethborne@gmail.com>
2018-04-23 16:59:54 -0700

    Training - Udemy - JavaScript - Modern React with Redux - Section 01 - Lecture 14 - Update Commit

---
+++ Udemy/JavaScript_Modern React with Redux/01_Section/14_Lecture/src/index.js
@@ -0,0 +1,22 @@
+// get React as a JS module
+import React from 'react';
+// reactDOm
+import ReactDOM from 'react-dom'
+// import searchBar - needs a file path reference. Don't need .js if it is a .js file.
+import SearchBar from './components/search_bar'
+// import API Key
+import getAPIKey from './api_key/api_key'
+// api key
+const API_KEY = getAPIKey();
+
+// create a new component.  this component should produce some HTML
+
+const App = () => (
+        <div>
+            <SearchBar />
+        </div>
+    );
+
+// take this components generated HTML and put it in the DOM
+// React render App
+ReactDOM.render(<App />, document.querySelector('.container'));
