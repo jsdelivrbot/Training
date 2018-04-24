bd0a45bd81e9a7ed497887e877c3479f1db12b87
Seth Borne <sethborne@gmail.com>
2018-04-23 17:02:20 -0700

    Training - Udemy - JavaScript - Modern React with Redux - Section 02 - Lecture 28 - First Commit

---
+++ Udemy/JavaScript_Modern React with Redux/02_Section/28_Lecture/src/index.js
@@ -0,0 +1,56 @@
+// get React as a JS module
+import React, {Component} from 'react';
+// reactDOm
+import ReactDOM from 'react-dom'
+// import searchBar - needs a file path reference. Don't need .js if it is a .js file.
+import SearchBar from './components/search_bar'
+// import API Key
+import getAPIKey from './api_key/api_key'
+// api key
+const API_KEY = getAPIKey();
+// npm youtube api search
+import YTSearch from 'youtube-api-search';
+// import video List
+import VideoList from './components/video_list'
+// import video detail
+import VideoDetail from './components/video_detail';
+
+
+// create a new component.  this component should produce some HTML
+// from functional to class.  will keep track of the videos on its state....
+class App extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = { videos: [] };
+    
+    // so see data right away
+    // fetching data
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+      // console.log(data);
+      this.setState({ videos: data })
+    })
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        {/* add VideoList */}
+        {/* videos will be passed as props to video_list.js */}
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
+}
+
+// take this components generated HTML and put it in the DOM
+// React render App
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+// Steps for converting from function component to class component
+// 1.  need to make sure to import Component from React (or use React.Component...)
+// 2.  redefine the function from const to class
+// 3.  extend the function with Component to get its methods
+// 4.  add a render method, put your data in there.
+// 5.  constructor(if needed, but probably needed, why else would you convert?)
