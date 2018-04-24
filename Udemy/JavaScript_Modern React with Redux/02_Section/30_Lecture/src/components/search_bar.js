fe36736709cffb7406ba3a1826867800e3755248
Seth Borne <sethborne@gmail.com>
2018-04-23 17:02:43 -0700

    Training - Udemy - JavaScript - Modern React with Redux - Section 02 - Lecture 30 - First Commit

---
+++ Udemy/JavaScript_Modern React with Redux/02_Section/30_Lecture/src/components/search_bar.js
@@ -0,0 +1,46 @@
+// import React from 'react';
+// importing Component in this way is the same as const Component = React.Component
+import React, { Component } from 'react';
+
+// ===============================
+// const SearchBar = () => {
+//     return <input />;
+// }
+// export default SearchBar;
+// ===============================^^ Functional Component
+
+
+// ========================== This is a Class Component
+// define class SearchBar and give it access to all the functionality of React.Component.
+// class SearchBar extends React.Component {
+// simplify the extendes React.Component by importing it on top
+class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    // input will be the value of term
+    this.state = { term: 'Start Value' }
+  }
+  // need a render method on a class based component.
+  render() {
+    return (
+      // return <input onChange={this.onInputChange} /> ;
+      // return <input onChange={event => console.log(event.target.value)} /> ;
+      <div className="search-bar">
+        <input 
+          value={this.state['term']}
+          onChange={event => this.setState({ term: event.target.value })}
+          />
+        {/* Value of the Input: {this.state['term']} */}
+      </div>
+    )
+  }
+  
+  // onInputChange(event){
+  //   // const inputVal = event.target.value;
+  //   // console.log(event.target.value);
+  //   console.log(event.target);
+  // }
+}
+// ========================== ^^ Class Component
+
+export default SearchBar;
