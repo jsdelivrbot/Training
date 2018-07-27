8c3f36f770a81a313ab2896f4f2aa94a3986d6f0
Seth Borne <sethborne@gmail.com>
2018-07-27 08:22:19 -0700

    Training - Udemy - JavaScript - Modern React with Redux - Section 01 - Lecture 07 - Update Commit

--- Udemy/JavaScript_Modern React with Redux/01_Section/07_Lecture/src/index.js
+++ Udemy/JavaScript_Modern React with Redux/01_Section/07_Lecture/src/index.js
@@ -1,8 +1,7 @@
 // create a new component.  this component should produce some HTML
 
-const App = function(){
-    return <div>Hi!</div>
+const App = function () {
+  return <div>Hi!</div>
 }
 
 // take this components generated HTML and put it in the DOM
-
