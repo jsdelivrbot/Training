d8f53a4e18d61f1c82281a6c95d0376ed8dde649
Seth Borne <sethborne@gmail.com>
2018-07-29 22:03:19 -0700

    Training - Udemy - JavaScript - Modern React with Redux - Section 05 - Lecture 52 - Update Commit

--- Udemy/JavaScript_Modern React with Redux/05_Section/52_Lecture/src/containers/search_bar.js
+++ Udemy/JavaScript_Modern React with Redux/05_Section/52_Lecture/src/containers/search_bar.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-  render(){
+  // state = {  }
+  render() {
     return (
       <form className="input-group">
         <input />
         <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary">
-            Submit
-          </button>
+          <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
       </form>
-    )
+    );
   }
 }
