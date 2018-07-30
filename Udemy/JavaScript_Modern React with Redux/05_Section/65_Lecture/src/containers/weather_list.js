94ed101bffab54ecc1a069e1d5a91a36f065b3a6
Seth Borne <sethborne@gmail.com>
2018-07-29 22:06:19 -0700

    Training - Udemy - JavaScript - Modern React with Redux - Section 05 - Lecture 65 - First Commit

---
+++ Udemy/JavaScript_Modern React with Redux/05_Section/65_Lecture/src/containers/weather_list.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine} from 'react-sparklines';
+import WeatherChart from '../components/weather_chart'
+
+class WeatherList extends Component {
+  // render weather items
+  renderWeather(cityData){
+    const cityName = cityData.city.name
+    const cityTemp = cityData.list.map(weather => weather.main.temp)
+    const cityPressure = cityData.list.map(weather => weather.main.pressure)
+    const cityHumidity = cityData.list.map(weather => weather.main.humidity)
+    // console.log(cityTemp, cityPressure, cityHumidity);
+    return (
+      <tr key={cityName}>
+        <td>{cityName}</td>
+        <td>
+          <WeatherChart data={cityTemp} color="orange" />
+        </td>
+      </tr>
+    );
+  }
+  
+  // state = {  }
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* rows are my data */}
+          {/* // js var */}
+          {
+            this.props.weather.map(this.renderWeather)
+          }
+        </tbody>
+      </table>
+    );
+  }
+}
+
+// { weather } = const weather = state.weather
+function mapStateToProps({ weather }){
+  // weather key is in the reducer...thats why weather
+  // return { weather: state.weather (or weather) } --\/ below is same for new param
+  return { weather }
+}
+
+export default connect(mapStateToProps)(WeatherList);
