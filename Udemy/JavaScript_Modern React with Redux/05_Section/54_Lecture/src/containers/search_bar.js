cee68543ebb3c5f4deefad1d4b18865cc91a4b5e
Seth Borne <sethborne@gmail.com>
2018-07-29 22:04:34 -0700

    Training - Udemy - JavaScript - Modern React with Redux - Section 05 - Lecture 54 - First Commit

---
+++ Udemy/JavaScript_Modern React with Redux/05_Section/54_Lecture/src/containers/search_bar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    // default to empty string
+    this.state = { searchTerm: '' };
+  
+    // bind this
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+  // change Handler
+  
+  onInputChange(event){
+    console.log(event.target.value);
+    this.setState( {searchTerm: event.target.value} )
+  }
+  
+  onFormSubmit(event){
+    event.preventDefault();
+    // go fetch weather data!
+  }
+  
+  render() {
+    return (
+      <form className="input-group" onSubmit={this.onFormSubmit}>
+        <input 
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.searchTerm}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
