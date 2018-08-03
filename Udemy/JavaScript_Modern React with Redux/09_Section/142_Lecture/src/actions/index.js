6ded2956f5bc0c2a24126770a7579826474c2976
Seth Borne <sethborne@gmail.com>
2018-08-03 09:27:49 -0700

    Training - Udemy - JavaScript - Modern React with Redux - Section 09 - Lecture 142 - First Commit

---
+++ Udemy/JavaScript_Modern React with Redux/09_Section/142_Lecture/src/actions/index.js
@@ -0,0 +1,26 @@
+// need axios and redux promise
+import axios from 'axios';
+
+export const FETCH_POSTS = 'fetch_posts'
+export const CREATE_POST = 'create_post'
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
+const API_KEY = '?key=ralphie1234'
+
+export function fetchPosts(){
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+  console.log(request);
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  }
+}
+
+export function createPost(values){
+  console.log(here);
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
+}
---
+++ Udemy/JavaScript_Modern React with Redux/09_Section/142_Lecture/src/components/posts_new.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+
+class PostsNew extends Component {
+  renderField(field){
+    const inputClassName = `form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ''}`;
+    return(
+      <div className={inputClassName}>
+        <label>{field.label}</label>
+        <input className="form-control"
+          type="text"
+          {...field.input}
+        />
+        <div className="text-help">
+          {field.meta.touched ? field.meta.error : ''}
+        </div>
+      </div>
+    )
+  }
+  onSubmit(values){
+    // console.log(values);
+    this.props.createPost(values)
+  }
+  
+  render(){
+    const { handleSubmit } = this.props;
+    return (
+      <div>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+          <Field 
+            label="Title for Post"
+            name="title"
+            component={this.renderField}
+          />
+          <Field 
+            label="Categories"
+            name="categories"
+            component={this.renderField}
+          />
+          <Field 
+            label="Post Content"
+            name="content"
+            component={this.renderField}
+          />
+          <button type="submit" className="btn btn-primary">Submit</button>
+          <Link className="btn btn-danger" to="/">
+            Return to Index
+          </Link>
+        </form>
+      </div>
+    );
+  }
+}
+
+function validate(values){
+  const errors = {};
+  // if no title, then add a title to error object and fail the validation
+  if(!values.title){
+    errors.title = 'Please Enter a Valid Title'
+  }
+  if(!values.categories){
+    errors.categories = 'Please Enter a Valid Category'
+  }
+  if(!values.content){
+    errors.content = 'Please Enter Valid Content'
+  }
+  
+  return errors;
+}
+
+export default reduxForm({
+  validate: validate,
+  form: 'PostsNewForm'
+})(
+  connect(null, {createPost})(PostsNew)
+);
