88612911a9da2e1c0e1b166eecfc8f0b571a8017
Seth Borne <sethborne@gmail.com>
2018-08-03 09:28:06 -0700

    Training - Udemy - JavaScript - Modern React with Redux - Section 09 - Lecture 143 - First Commit

---
+++ Udemy/JavaScript_Modern React with Redux/09_Section/143_Lecture/src/reducers/reducer_posts.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+
+import { FETCH_POSTS } from '../actions';
+// we want an object, so init obj
+export default function(state = {}, action){
+  switch (action.type) {
+    case FETCH_POSTS:
+      // console.log(action.payload.data);
+      // lodash - take array of records, and take a prop out of a obj, make key, make val the obj
+      return _.mapKeys(action.payload.data, 'id');
+  
+    default:
+      return state;
+  }
+}
