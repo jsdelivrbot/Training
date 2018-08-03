ac548153b16e6d0caf98101b0497061194b33b71
Seth Borne <sethborne@gmail.com>
2018-08-02 19:04:46 -0700

    Training - Udemy - JavaScript - Modern React with Redux - Section 09  - Lecture 133 - First Commit

---
+++ Udemy/JavaScript_Modern React with Redux/09_Section/133_Lecture/src/components/posts_new.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form'
+
+class PostsNew extends Component {
+  renderTitleField(){
+  }
+  render(){
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/">
+            Return to Index
+          </Link>
+        </div>
+        <form action="">
+          <Field 
+            name="title"
+            component={this.renderTitleField}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew) ;
